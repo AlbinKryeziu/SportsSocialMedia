540fc0e8b70a0543d5439b1ba12692adaaae2819
AlbinKryeziu <57589969+AlbinKryeziu@users.noreply.github.com>
2021-02-26 17:19:55 +0100

    fixes

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -1,7 +1,8 @@
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
     <div>
-        <img class="mb-4" src="images/logo-crop.png" alt="" width="100px">
+        <img class="mb-4" src="{{ asset('images/logo/final-logo.png') }}" alt="" width="200px">
     </div>
+    
 
     <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
